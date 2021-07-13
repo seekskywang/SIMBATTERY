705d92832b172d74b1516f74d3544a9070dc9956
seekskywang <seekskywang@gmail.com>
2021-07-13 09:35:43 +0800

    1

--- Project/Output/globalvalue.d
+++ Project/Output/globalvalue.d
@@ -1,4 +1,4 @@
 .\output\globalvalue.o: ..\User\Globalvalue\GlobalValue.c
 .\output\globalvalue.o: ..\User\Globalvalue\TypeDefine.h
 .\output\globalvalue.o: ..\User\Globalvalue/GlobalValue.h
-.\output\globalvalue.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\output\globalvalue.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
